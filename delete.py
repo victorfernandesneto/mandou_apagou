5e9ecb4918f493aa4047284f501afdbb95ee21ea
Victor <vvfernandesneto@gmail.com>
2023-11-06 09:28:18 -0300

    upgrade no bot pra utilizar uma database, de forma que diversos outros canais possam usar

--- delete.py
+++ delete.py
@@ -1,13 +1,28 @@
 import discord
 from discord.ext import commands
+import typing
 import os
 from dotenv import load_dotenv
+import psycopg2
+
 
 load_dotenv()
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+cur.execute('SELECT * FROM channels')
+data = cur.fetchall()
+channels_dict = {}
+for item in data:
+    channels_dict[item[0]] = item[1]
+print(channels_dict)
+cur.close()
+conn.close()
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-channels_list = []
 
 
 @bot.event
@@ -17,22 +32,36 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.channel.id in channels_list:
+    channel_id = str(message.channel.id)
+    if channel_id in channels_dict:
         if message.author == bot.user:
             return
         else:
-            await message.delete(delay=60)
+            delete_time = channels_dict[channel_id]
+            print(delete_time)
+            await message.delete(delay=delete_time)
     await bot.process_commands(message)
 
 
 @bot.command()
-async def here(ctx):
-    channel_id = ctx.channel.id
-    if channel_id in channels_list:
-        channels_list.remove(channel_id)
-        await ctx.send('Aqui me despeço!')
+async def here(ctx, time: typing.Optional[int] = 60):
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    channel_id = str(ctx.channel.id)
+    await ctx.message.delete()
+    if channel_id in channels_dict:
+        cur.execute('DELETE FROM channels WHERE channel_id = %s', (channel_id,))
+        del channels_dict[channel_id]
+        print(channels_dict)
+        await ctx.send("I'm out!", delete_after=5.0)
     else:
-        await ctx.send('Vou deletar geral, fui!')
-        channels_list.append(channel_id)
+        cur.execute('INSERT INTO channels (channel_id, time) VALUES (%s, %s)', (channel_id, time,))
+        channels_dict[channel_id] = time
+        print(channels_dict)
+        await ctx.send("You haven't seen nothing!", delete_after=5.0)
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 bot.run(str(os.environ.get('DISCORD_TOKEN')))
