9512a89f83d8dba80c579c85706fa160605872f0
Victor <vvfernandesneto@gmail.com>
2023-11-05 21:39:41 -0300

    organizado de forma que ele acesse diversos canais

--- delete.py
+++ delete.py
@@ -1,21 +1,38 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
-from discord.ext import commands
 
 load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+channels_list = []
+
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+
 @bot.event
 async def on_message(message):
-    if message.channel.id == int((os.environ.get('DISCORD_CHANNEL'))):
-        await message.delete(delay=60)
+    if message.channel.id in channels_list:
+        if message.author == bot.user:
+            return
+        else:
+            await message.delete(delay=60)
+    await bot.process_commands(message)
 
-print(str(os.environ.get('DISCORD_TOKEN')))
+
+@bot.command()
+async def here(ctx):
+    channel_id = ctx.channel.id
+    if channel_id in channels_list:
+        channels_list.remove(channel_id)
+        await ctx.send('Aqui me despeço!')
+    else:
+        await ctx.send('Vou deletar geral, fui!')
+        channels_list.append(channel_id)
+
 bot.run(str(os.environ.get('DISCORD_TOKEN')))
